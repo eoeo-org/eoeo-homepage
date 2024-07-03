2cbc659b29db38258c45f7e8b4a6e80a8ed9be1b
c30 <43949163+Zel9278@users.noreply.github.com>
2024-07-04 00:21:28 +0900

    change to "eoeo"

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-const jText = document.getElementById("jText")
+const eoeoText = document.getElementById("eoeoText")
 
 let texts = [
-    'j-eoeo',
-    'jjjj',
-    'jenkins',
-    'jello jorld',
-    'jazujazu123123',
-    'j eoeo is powered by tty7.uk'
+    'eoeo-org',
+    'eoeo',
+    'eoello eorld',
+    'eoeo123123',
+    'eoeo org is powered by c30.life'
 ];
 
 function getRandomText(texts) {
@@ -20,16 +19,16 @@ function getRandomTime(min, max) {
 
 async function runRandomTextFrames() {
     while (true) {
-        jText.innerHTML = '';
+        eoeoText.innerHTML = '';
         const text = getRandomText(texts);
         for (const char of text) {
-            jText.innerHTML += char;
+            eoeoText.innerHTML += char;
             await new Promise(resolve => setTimeout(resolve, getRandomTime(100, 500)));
         }
         await new Promise(resolve => setTimeout(resolve, 1000));
 
         for (let i = text.length; i >= 0; i--) {
-            jText.innerHTML = text.substring(0, i) || 'j';
+            eoeoText.innerHTML = text.substring(0, i) || 'j';
             await new Promise(resolve => setTimeout(resolve, getRandomTime(100, 500)));
         }
 
